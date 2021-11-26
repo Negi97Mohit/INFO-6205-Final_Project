3c474adeb7297d888c8b4e2b7a5a69e17e068d8e
Mohit Negi <62002911+Negi97Mohit@users.noreply.github.com>
2021-11-26 00:46:09 -0500

    Benchmarking for sorting algo implemented

---
+++ src/main/java/edu/neu/coe/info6205/FinalProject/Insertion.java
@@ -0,0 +1,22 @@
+package edu.neu.coe.info6205.FinalProject;
+
+
+public class Insertion {
+
+    public static void sort(String[] a, int lo, int hi, int d) {
+        // Sort from a[lo] to a[hi], starting at the dth character.
+        for (int i = lo; i <= hi; i++)
+            for (int j = i; j > lo && less(a[j], a[j - 1], d); j--)
+                exch(a, j, j - 1);
+    }
+
+    private static boolean less(String v, String w, int d) {
+        return v.substring(d).compareTo(w.substring(d)) < 0;
+    }
+
+    private static void exch(String[] a, int i, int j) {
+        String t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+}
