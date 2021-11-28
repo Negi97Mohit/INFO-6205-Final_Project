d87be41e990e3e3a80a863181e6a2473f2d83103
Mohit Negi <62002911+Negi97Mohit@users.noreply.github.com>
2021-11-28 16:11:43 -0500

    Revert "updated gitignore and SortBenchmarkHelper"
    This reverts commit eb9239f5a09743f1d95c0d8eb8c63ce8cd62dc2c.

--- src/main/java/edu/neu/coe/info6205/util/SortBenchmarkHelper.java
+++ src/main/java/edu/neu/coe/info6205/util/SortBenchmarkHelper.java
@@ -61,6 +61,7 @@ public class SortBenchmarkHelper {
         if (matcher.find()) {
             final String word = matcher.group(1);
             final String[] strings = word.split("[\\s\\p{Punct}\\uFF0C]");
+            System.out.println(strings.length);
             return Arrays.asList(strings);
         } else
             return new ArrayList<>();
