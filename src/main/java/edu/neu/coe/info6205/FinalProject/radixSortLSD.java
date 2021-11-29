73564004f255a3291689bfb99a9421a961c6a0ee
Mohit Negi <62002911+Negi97Mohit@users.noreply.github.com>
2021-11-28 21:55:35 -0500

    LSD Radix Sort Implmented

--- src/main/java/edu/neu/coe/info6205/FinalProject/radixSortLSD.java
+++ src/main/java/edu/neu/coe/info6205/FinalProject/radixSortLSD.java
@@ -3,32 +3,96 @@ package edu.neu.coe.info6205.FinalProject;
 import java.io.FileNotFoundException;
 
 public class radixSortLSD <X extends Comparable<X>>{
+
+    private static final int BITS_PER_BYTE = 8;
     public static void main(String[] args) throws FileNotFoundException {
         String resource="chinese_names.txt";
         String[] pin=toEng.generateList(resource);
         String[] chiToEng=new String[pin.length];
+        for (int i = 0; i < pin.length; i++) {
+            chiToEng[i] = regexMatch.getPingYin(pin[i]);
+        }
 
-        for (int i = 0; i < pin.length; i++) { chiToEng[i] = regexMatch.getPingYin(pin[i]); }
-        for (String word:chiToEng) System.out.println(word.length());
+        int min=10;
+        for (String word:chiToEng) {
+            if(word.length()<min)
+                min=word.length();
+        }
+        int n=chiToEng.length;
+        int w=min;
+        for (int i=0;i<n;i++)
+            assert chiToEng[i].length()==w:"String same length";
+
+        sort(chiToEng,w);
+
     }
-    public static void sort(String[] a, int W)
-    {
-        int R = 256;
-        int N = a.length;
-        String[] aux = new String[N];
-        for (int d = W-1; d >= 0; d--)
-        {
+    public static void sort(String[] a, int w) {
+        int n = a.length;
+        int R = 256;   // extend ASCII alphabet size
+        String[] aux = new String[n];
+
+        for (int d = w-1; d >= 0; d--) {
+            // sort by key-indexed counting on dth character
+
+            // compute frequency counts
             int[] count = new int[R+1];
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < n; i++)
                 count[a[i].charAt(d) + 1]++;
+
+            // compute cumulates
             for (int r = 0; r < R; r++)
                 count[r+1] += count[r];
-            for (int i = 0; i < N; i++)
+
+            // move data
+            for (int i = 0; i < n; i++)
                 aux[count[a[i].charAt(d)]++] = a[i];
-            for (int i = 0; i < N; i++)
+
+            // copy back
+            for (int i = 0; i < n; i++)
                 a[i] = aux[i];
         }
     }
+    public static void sort(int[] a) {
+        final int BITS = 32;                 // each int is 32 bits
+        final int R = 1 << BITS_PER_BYTE;    // each bytes is between 0 and 255
+        final int MASK = R - 1;              // 0xFF
+        final int w = BITS / BITS_PER_BYTE;  // each int is 4 bytes
 
+        int n = a.length;
+        int[] aux = new int[n];
 
+        for (int d = 0; d < w; d++) {
+
+            // compute frequency counts
+            int[] count = new int[R+1];
+            for (int i = 0; i < n; i++) {
+                int c = (a[i] >> BITS_PER_BYTE*d) & MASK;
+                count[c + 1]++;
+            }
+
+            // compute cumulates
+            for (int r = 0; r < R; r++)
+                count[r+1] += count[r];
+
+            // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
+            if (d == w-1) {
+                int shift1 = count[R] - count[R/2];
+                int shift2 = count[R/2];
+                for (int r = 0; r < R/2; r++)
+                    count[r] += shift1;
+                for (int r = R/2; r < R; r++)
+                    count[r] -= shift2;
+            }
+
+            // move data
+            for (int i = 0; i < n; i++) {
+                int c = (a[i] >> BITS_PER_BYTE*d) & MASK;
+                aux[count[c]++] = a[i];
+            }
+
+            // copy back
+            for (int i = 0; i < n; i++)
+                a[i] = aux[i];
+        }
+    }
 }
